0f0ff938b61e39b7d7536807521d2668e85e80db
savita madray <savitamadray01@gmail.com>
2017-10-04 03:09:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function deleteFromObjectByKey(object, key){
 var obj = { prop: 1 }
 var newObj = {obj: 'prop'};
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete obj.prop, [];
+  delete obj.prop;
   return obj;
 }
